bf7f896af7a2973b88e19d93953b787ec9fccfc7
huynq1992hpvn <huynq1992hpvn@gmail.com>
2023-06-05 01:16:44 +0700

    lan1

---
+++ change.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phuongthucchange
+{
+    public class Student
+    {
+        private int rollno ;
+        private string name ;
+        private static string college = "BBDTT";
+
+        public Student(int rollno = 0,  string name = "huy")
+        {
+            this.rollno = rollno;
+            this.name = name;
+        }
+        public static void Change()
+        {
+            college = "CODEGYM";
+        }
+        public void Display()
+        {
+            Console.WriteLine(rollno + " " + name + " " + college);
+        }
+    }
+}
